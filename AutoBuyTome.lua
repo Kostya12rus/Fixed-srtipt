e441fe8158b53da28d0cbbcbb7016f9fd845f2a0
Kostya12rus <mkg97@yandex.ru>
2018-06-24 20:51:52 +0300

    asfaf

---
+++ AutoBuyTome.lua
@@ -0,0 +1,27 @@
+local AutoBuyBock = {}
+
+AutoBuyBock.Enable = Menu.AddOption({ "Kostya12rus"}, "Auto Buy Tome of Knowledge", "Buy every 10 minuts Tome of Knowledge if you have 150 gold")
+
+function AutoBuyBock.OnUpdate()
+	if not Menu.IsEnabled(AutoBuyBock.Enable) then return end
+ 	local myHero = Heroes.GetLocal()
+	if not myHero then return end
+	if GameRules.GetGameTime() > AutoBuyBock.Delay and not NPC.HasItem(myHero, "item_tome_of_knowledge") then
+		local realtime = (GameRules.GetGameTime() - GameRules.GetGameStartTime()) / 60 % 10
+		if (realtime > 0 and realtime < 0.2) then
+			Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, 257, Vector(0, 0, 0), 257, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, myHero, false, true)
+		end
+		AutoBuyBock.Delay = GameRules.GetGameTime() + 1
+	end
+end
+
+function AutoBuyBock.init()
+	AutoBuyBock.Delay = 0
+end
+
+function AutoBuyBock.OnGameStart()
+	AutoBuyBock.init()
+end
+AutoBuyBock.init()
+
+return AutoBuyBock
